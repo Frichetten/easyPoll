dafb750a22d13e5018971d346f069ce286c4bf5c
Nick Frichette <frichetten@gmail.com>
2016-10-05 09:33:05 -0500

    First commit. Implemented basic login features. Nothing will display, but it will connect to the backend and either login, or signup a user. Need more testing to see if login works

---
+++ app.js
@@ -0,0 +1,50 @@
+(function () {
+
+console.log("Initializing");
+
+
+ console.log("Getting Elements");   
+//Get elements
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignup = document.getElementById('btnSignup');
+    const btnLogout = document.getElementById('btnLogout');
+    
+console.log("Creating login");    
+//Add login event
+    btnLogin.addEventListener('click', e => {
+        //Get email and pass
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        //sign in
+        const promise = auth.signInWithEmailAndPassword(email,pass);
+        promise.catch(e => console.log(e.message));
+    });
+
+console.log("Creating singup");
+//Add signup event
+    btnSignup.addEventListener('click', e=> {
+          //Get email and pass
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        //sign in
+        const promise = auth.createUserWithEmailAndPassword(email,pass);
+        promise.catch(e => console.log(e.message));
+    });
+    
+console.log("Creating state listener");
+//Add realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log(firebaseUser);
+            btnLogout.classList.remove('hide');
+        } else {
+            console.log('not logged in');
+            btnLogout.classList.add('hide');
+        }
+    });
+    
+}());
