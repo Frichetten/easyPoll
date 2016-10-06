baf2dd860e32bb296b62c04dcdd3333e0c6ec03a
Nick Frichette <frichetten@gmail.com>
2016-10-05 20:55:17 -0500

    Cleaned the heck out of it, added comments. Included the ability to display who the active user is

--- app.js
+++ app.js
@@ -1,57 +1,53 @@
 (function () {
-
-console.log("Initializing");
-
-
- console.log("Getting Elements");   
-//Get elements
+    console.log("Getting Elements");   
+    //Get elements
     const txtEmail = document.getElementById('txtEmail');
     const txtPassword = document.getElementById('txtPassword');
     const btnLogin = document.getElementById('btnLogin');
     const btnSignup = document.getElementById('btnSignup');
     const btnLogout = document.getElementById('btnLogout');
     
-console.log("Creating login");    
-//Add login event
+    console.log("Creating Login");    
     btnLogin.addEventListener('click', e => {
-        //Get email and pass
+        //Get Email and Password from Input
         const email = txtEmail.value;
         const pass = txtPassword.value;
         const auth = firebase.auth();
-        //sign in
+        
+        //Sign in with Email and Password
         const promise = auth.signInWithEmailAndPassword(email,pass);
-        console.log("Logging in");
+        console.log("Logging In");
         promise.catch(e => console.log(e.message));
         firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
                 console.log(firebase.auth().currentUser);
+                //Successful Login, go to Profile Page
                 document.location.href="profile.html";
             } else {
-                // No user is signed in.
+                console.log("Failed to Sign in");
             }
         });
     });
 
-console.log("Creating singup");
-//Add signup event
+    console.log("Creating Signup");
     btnSignup.addEventListener('click', e=> {
-          //Get email and pass
+        //Get email and pass
         const email = txtEmail.value;
         const pass = txtPassword.value;
         const auth = firebase.auth();
-        //sign in
+        //Creating account
         const promise = auth.createUserWithEmailAndPassword(email,pass);
         promise.catch(e => console.log(e.message));
+        console.log("Account Created");
     });
     
-console.log("Creating state listener");
-//Add realtime listener
+    console.log("Creating Logout");
     btnLogout.addEventListener('click', e=> {
        //Logout
         const auth = firebase.auth();
         const promise = auth.signOut();
         promise.catch(e => console.log(e.message));
-        console.log("logged out");
+        console.log("Logged Out");
     });
     
 }());
