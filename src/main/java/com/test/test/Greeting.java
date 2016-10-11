bcadfdc495f83276e0ea497d5d2a6c4ff657767d
Nick Frichette <frichetten@gmail.com>
2016-10-11 15:53:27 -0500

    First commit

---
+++ src/main/java/com/test/test/Greeting.java
@@ -0,0 +1,28 @@
+package com.test.test;
+
+public class Greeting {
+
+    private long id;
+    private String content;
+    
+    public Greeting(){
+    	
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
