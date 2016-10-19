11f77468fa403bfd7d65f0c5679c6657d062fa9e
Nick Frichette <frichetten@gmail.com>
2016-10-18 22:04:48 -0500

    Basic login functionality has been added along with some of the front end

--- src/main/java/com/test/test/HomeController.java
+++ src/main/java/com/test/test/HomeController.java
@@ -29,23 +29,13 @@ public class HomeController {
 	public ModelAndView addUser(@ModelAttribute("SpringWeb")User user, ModelMap model,
 			HttpServletRequest request) {
 		//Add these attributes to the model so they will appear
-		//In the "result" page
-		if(user.getUsername().equals("Administrator") && user.getPassword().equals("easyPoll")){
 		model.addAttribute("username", user.getUsername());
 		model.addAttribute("password", user.getPassword());
 		System.out.println(user.getUsername());
 		System.out.println(user.getPassword());
 		//This token will be the session attribute
 		request.getSession().setAttribute("token", user);
-<<<<<<< HEAD
 		return new ModelAndView("home");
-=======
-		return new ModelAndView("result");
-		}
-		else{
-			return new ModelAndView("user", "command", new User());
-		}
->>>>>>> 5d7d0e6f96d9ac686be53cb123824611bfbf7439
 	}
 	
 	@RequestMapping(value = "/greeting", method = RequestMethod.GET)
