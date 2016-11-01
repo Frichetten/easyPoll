40fae8760c61a8ca640eb371234ae56974870525
Nick Frichette <frichetten@gmail.com>
2016-11-01 14:29:18 -0500

    Added pages

--- src/main/java/com/test/test/HomeController.java
+++ src/main/java/com/test/test/HomeController.java
@@ -7,6 +7,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
@@ -31,8 +37,15 @@ public class HomeController {
 		//Add these attributes to the model so they will appear
 		model.addAttribute("username", user.getUsername());
 		model.addAttribute("password", user.getPassword());
-		System.out.println(user.getUsername());
-		System.out.println(user.getPassword());
+		Firebase nick = new Firebase("https://testproject-9f072.firebaseio.com");
+		
+	    nick.child("Users/User/Password").addValueEventListener(new ValueEventListener() {
+	        @Override
+	        public void onDataChange(DataSnapshot snapshot) {
+	          System.out.println(snapshot.getValue());  //prints "Do you have data? You'll love Firebase."
+	        }
+	        @Override public void onCancelled(FirebaseError error) { }
+	      });
 		//This token will be the session attribute
 		request.getSession().setAttribute("token", user);
 		return new ModelAndView("home");
@@ -49,5 +62,23 @@ public class HomeController {
 		System.out.println("Username: " + a.getUsername());
 		System.out.println("Password: " + a.getPassword());
 		return new ModelAndView("greeting");
+	}
+	
+	@RequestMapping(value = "/communitypolls", method = RequestMethod.GET)
+	public ModelAndView community(@ModelAttribute("SpringWeb")User user, ModelMap model,
+			HttpServletRequest request){
+		return new ModelAndView("communitypolls");
+	}
+	
+	@RequestMapping(value = "/createpoll", method = RequestMethod.GET)
+	public ModelAndView createpoll(@ModelAttribute("SpringWeb")User user, ModelMap model,
+			HttpServletRequest request){
+		return new ModelAndView("createpoll");
+	}
+	
+	@RequestMapping(value = "/singlepoll", method = RequestMethod.GET)
+	public ModelAndView singlePoll(@ModelAttribute("SpringWeb")User user, ModelMap model,
+			HttpServletRequest request){
+		return new ModelAndView("singlepoll");
 	}
 }
