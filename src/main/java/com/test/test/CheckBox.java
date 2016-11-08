62f7c99ee9813a01fd2d9c1a0320e2d06bc156d7
LAPTOP-0QLVF3CT\dalle <kwdalle@ilstu.edu>
2016-11-08 16:04:58 -0600

    First commit to this branch

---
+++ src/main/java/com/test/test/CheckBox.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.test.test;
+
+/**
+ * @author dalle
+ *
+ */
+public class CheckBox implements Answer {
+
+	/* (non-Javadoc)
+	 * @see com.test.test.Answer#getAnswerOptions(java.lang.String)
+	 */
+	@Override
+	public String[] getAnswerOptions(String textQuestion) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
