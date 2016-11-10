a25b91859fcc1d25cd4f490f94d587b38a3d7cf1
LAPTOP-0QLVF3CT\dalle <kwdalle@ilstu.edu>
2016-11-10 14:56:42 -0600

    More Backend changes

--- src/main/java/com/test/test/CheckBox.java
+++ src/main/java/com/test/test/CheckBox.java
@@ -12,11 +12,6 @@ public class CheckBox implements Answer {
 	/* (non-Javadoc)
 	 * @see com.test.test.Answer#getAnswerOptions(java.lang.String)
 	 */
-	@Override
-	public String[] getAnswerOptions(String textQuestion) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public String[] getAnswers(int PollNum) throws Exception {
