c8e8bacd56a9fb7be70b1b98ece1047628f2c7ac
LAPTOP-0QLVF3CT\dalle <kwdalle@ilstu.edu>
2016-11-21 13:13:59 -0600

    finished the Group.java file

--- src/main/java/com/test/test/Group.java
+++ src/main/java/com/test/test/Group.java
@@ -16,6 +16,22 @@ public class Group {
 	private Poll groupPoll;
 	private ArrayList<String> members = new ArrayList<String>();
 	
+	// Allows for the creation of a group object with an already available member list
+	public Group(int groupID, User admin, Poll poll, ArrayList<String> mem){
+		this.groupID = groupID;
+		this.admin = admin;
+		this.groupPoll = poll;
+		this.members = mem;
+	}
+	
+	// This is for if you do not have a list of people already
+	public Group(int groupID, User admin, Poll poll)
+	{
+		this.groupID = groupID;
+		this.admin = admin;
+		this.groupPoll = poll;
+	}
+	
 	public ArrayList<String> getRegisteredUsers(){
 		return members;
 	}
@@ -30,5 +46,10 @@ public class Group {
 
 	public Poll getGroupPoll() {
 		return groupPoll;
+	}
+	
+	// This simple method is just adding someone to the group
+	public void invite(String newUser){
+		members.add(newUser);
 	}
 }
