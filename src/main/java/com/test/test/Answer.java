c16e08ca0b5d5bb6f1a2ce194a9ebbd29c916575
Nick Frichette <frichetten@gmail.com>
2016-12-03 13:42:52 -0600

    added comments to the answer class

--- src/main/java/com/test/test/Answer.java
+++ src/main/java/com/test/test/Answer.java
@@ -8,32 +8,39 @@ import java.util.ArrayList;
 
 public class Answer {
 	
-	//public String[] getAnswerOptions(String textQuestion) throws Exception;
-
+	//Private class variables
 	private boolean isRadio;
 	private ArrayList<String> answerOptions = null;
 	private ArrayList<Integer> answersChosen = null;
 	private String answerparams;
 	
-	public void setAnswerOptions(ArrayList<String> answerOptions){
-		this.answerOptions = answerOptions;
-	}
-	
-	public String getAnswerparams() {
-		return answerparams;
-	}
-	public void setAnswerparams(String answerparams) {
-		this.answerparams = answerparams;
-	}
+	/*
+	 * Default constructor. Spring MVC needs this DO NOT DELETE
+	 */
 	public Answer(){
 		
 	}
-	public void setAnswer(String answer){
-		this.answerparams = answer;
+	
+	/*
+	 * Answer object constructor
+	 */
+	public Answer(ArrayList<String> options, ArrayList<Integer> chosen, boolean isRadio){
+		this.answerOptions = options;
+		this.answersChosen = chosen;
+		this.isRadio = isRadio;
 	}
+	
+	/*
+	 * Will add the answer chosen
+	 */
 	public void addAnswerChosen(int index, int pollNum){
 		this.answersChosen.set(index, this.answersChosen.get(index)+1);
 		DBQuery.addAnswer(index+1, pollNum);
+	}
+	
+	//Getters and setters don't need better comments
+	public void setAnswer(String answer){
+		this.answerparams = answer;
 	}
 	public String getAnswer(){
 		return this.answerparams;
@@ -41,44 +48,22 @@ public class Answer {
 	public boolean getIsRadio(){
 		return this.isRadio;
 	}
-	public Answer(ArrayList<String> options, ArrayList<Integer> chosen, boolean isRadio){
-
-		this.answerOptions = options;
-		this.answersChosen = chosen;
-		this.isRadio = isRadio;
-	}
-	
 	public ArrayList<Integer> getAnswerChosen(){
 		return this.answersChosen;
 	}
-	
 	public ArrayList<String> getAnswerOptions(){
 		return this.answerOptions;
 	}
-	
-	/*
-	public String[] getAnswerOptions(int Pollnum, String username) throws SQLException{
-		Connection Dbcon = DBConnection.getConnection();
-		
-		//String SQL = "SELECT PollNum FROM PollData WHERE Question = " + textQuestion;
-		Statement statement = Dbcon.createStatement();
-		ResultSet rs = statement.executeQuery(SQL);
-		
-		if (rs.next()){
-			System.out.println("PollNum: " + rs.getString(1));
-			//If Authentication successful
-			//Add these attributes to the model so they will appear
-		}
-		else {
-			
-		}
-		
-		
-		return null;
-	}
-	*/
 	public Answer getUserAnswer(int pollNum, String username){
 		return DBQuery.getUserAnswer(pollNum, username);
 	}
-
+	public void setAnswerOptions(ArrayList<String> answerOptions){
+		this.answerOptions = answerOptions;
+	}
+	public String getAnswerparams() {
+		return answerparams;
+	}
+	public void setAnswerparams(String answerparams) {
+		this.answerparams = answerparams;
+	}
 }
