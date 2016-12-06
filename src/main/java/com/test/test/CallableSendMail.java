ea3df2c6f518e03f73118fab015c3876e5a88312
mfletch1 <mfletch1@ilstu.edu>
2016-12-05 20:01:10 -0600

    added some comments to callable classes

--- src/main/java/com/test/test/CallableSendMail.java
+++ src/main/java/com/test/test/CallableSendMail.java
@@ -8,12 +8,16 @@ public class CallableSendMail implements Callable{
 	private String subject;
 	private String info;
 	
+	//constructor
 	public CallableSendMail(String email, String subject, String info){
 		this.address = email;
 		this.subject = subject;
 		this.info = info;
 	}
 
+	//thread function that sends emails to recommend a poll to someone.
+	//A thread is used to help speed up responsiveness after a user tries to recommend.
+	//It used to be that a user would have to wait for the email to send before the UI would refresh.
 	@Override
 	public Void call() throws Exception {
 		return Email.sendMail(address, subject, info);
--- src/main/java/com/test/test/CallableSendMassMail.java
+++ src/main/java/com/test/test/CallableSendMassMail.java
@@ -10,6 +10,9 @@ public class CallableSendMassMail implements Callable {
 		this.info = info;
 	}
 
+	//thread function that sends mass emails to all registered users.
+	//A thread is used to help speed up responsiveness after an admin sends out a newsletter.
+	//It used to be that an admin would have to wait for all the emails to send before the UI would refresh.
 	@Override
 	public Void call() throws Exception {
 
